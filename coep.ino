d460804e291b1bd2956a7083551c3b92e1444784
Yashveer27 <31110191+Yashveer27@users.noreply.github.com>
2017-09-17 17:21:09 +0530

    Add files via upload

---
+++ coep.ino
@@ -0,0 +1,29 @@
+int x;
+int count ,c=0 ;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(4,INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+x=digitalRead(4);
+//Serial.println(x);
+//delay(2000);
+
+if(x==1)
+{
+  count++;
+  
+}
+  if(count==1)
+   {
+    c++;
+    count=0;
+   }
+ Serial.println(c);
+    //Serial.write(c);
+    delay(3000);
+}
